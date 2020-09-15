d1224304f21e716540ec2d520799aa7cb13ae3e7
RickVellinga <55230960+RickVellinga@users.noreply.github.com>
2020-09-15 10:46:46 +0200

    Add files via upload

---
+++ testen.js
@@ -0,0 +1,31 @@
+function Student(id, klas, student) {
+  let studObj = this;
+  studObj.id = id;
+  studObj.klas = klas;
+  studObj.student = student;
+
+}
+
+
+
+
+let student1 = new Student('29215', 'MD2Aa', 'Rick');
+let student2 = new Student('29147', 'MD2Aa', 'Milo');
+let student3 = new Student('28462', 'MD2Aa', 'Daan');
+let student4 = new Student('29118', 'MD2Ab', 'Pepijn');
+let student5 = new Student('29253', 'MD2Ab', 'Teun');
+let student6 = new Student('12700', 'MD2Ab', 'Lau');
+let student7 = new Student('29743', 'MD2Ab', 'Bram');
+let student8 = new Student('28746', 'MD2Ab', 'Romer');
+let student9 = new Student('29093', 'MD2Aa', 'Max');
+
+
+console.log(student1);
+console.log(student2);
+console.log(student3);
+console.log(student4);
+console.log(student5);
+console.log(student6);
+console.log(student7);
+console.log(student8);
+console.log(student9);
